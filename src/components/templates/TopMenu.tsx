9df0f562d5e45887b569ed355e1603c2038628c2
Tonyk <tonykurosaki117@gmail.com>
2022-05-24 23:46:18 -0500

    changed top menu

--- src/components/templates/TopMenu.tsx
+++ src/components/templates/TopMenu.tsx
@@ -6,10 +6,10 @@ import { faArrowLeft, faCartShopping } from '@fortawesome/free-solid-svg-icons'
 const TopMenu: FC = () => {
   return(
     <>
-        <span className='fixed left-3 top-3 rounded-full bg-zinc-600 w-8 h-8 flex items-center justify-center'>
+        <span className='fixed left-3 top-3 rounded-full  w-8 h-8 flex items-center justify-center'>
           <FontAwesomeIcon icon={faArrowLeft} size="lg"/>
         </span>
-        <span className='fixed right-3 top-3 rounded-full bg-zinc-600 w-8 h-8 flex items-center justify-center'>
+        <span className='fixed right-3 top-3 rounded-full  w-8 h-8 flex items-center justify-center'>
           <FontAwesomeIcon icon={faCartShopping} size="lg"/>
         </span>
     </>
